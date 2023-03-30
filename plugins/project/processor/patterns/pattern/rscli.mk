9816a16a1cc308f1c1e655f6bab95a1796e102c0
AlexSkilled <olegthander@yandex.ru>
2023-03-30 17:36:39 +0300

    RSI-62: migrations

--- plugins/project/processor/patterns/pattern/rscli.mk
+++ plugins/project/processor/patterns/pattern/rscli.mk
@@ -1,3 +1,19 @@
 RSCLI_VERSION=v0.0.17-alpha
 rscli-version:
-	 @echo $(RSCLI_VERSION)
+	@echo $(RSCLI_VERSION)
+
+#==============
+# migrations
+#==============
+
+GOOSE_VERSION=$(shell goose -version)
+MIG_DIR="migrations/"
+goose-dep:
+ifeq ("$(GOOSE_VERSION)", "")
+	@echo "installing goose..."
+	@go install github.com/pressly/goose/v3/cmd/goose@latest
+else
+	@echo "goose is installed!"
+endif
+
+#==============
