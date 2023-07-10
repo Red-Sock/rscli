30bd578192642ec8de32541053cc7abd795d23cd
AlexSkilled <olegthander@yandex.ru>
2023-07-10 21:58:07 +0300

    [RSI-117]: configuration

--- internal/config/cfg.go
+++ internal/config/cfg.go
@@ -3,6 +3,7 @@ package config
 import (
 	_ "embed"
 	"os"
+	"path"
 
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v3"
@@ -10,12 +11,19 @@ import (
 	"github.com/Red-Sock/rscli/pkg/flag"
 )
 
-const configFilename = "rscli.yaml"
+const (
+	configFilename          = "rscli.yaml"
+	customPathToConfig      = "--rscli-cfg"
+	environmentPathToConfig = "RSCLI_CONFIG_PATH"
+)
 
-const customPathToConfig = "--rscli-cfg"
+const (
+	pathToConfig = "RSCLI_PATH_TO_CONFIG"
+	pathToMain   = "RSCLI_PATH_TO_MAIN"
+)
 
 //go:embed rscli.yaml
-var example []byte
+var builtInConfig []byte
 
 type RsCliConfig struct {
 	Env struct {
@@ -25,29 +33,74 @@ type RsCliConfig struct {
 }
 
 func GetConfig() (*RsCliConfig, error) {
-	flags := flag.ParseArgs(os.Args[1:])
-
-	cfgFilePath, err := flag.ExtractOneValueFromFlags(flags, customPathToConfig)
-	if err != nil {
-		return nil, errors.Wrap(err, "error extracting config value from arguments")
-	}
-
-	var file []byte
-	if cfgFilePath != "" {
-		file, err = os.ReadFile(cfgFilePath)
-		if err != nil {
-			return nil, errors.Wrap(err, "error reading file from FS")
-		}
-	} else {
-		file = example
-	}
-
-	var c RsCliConfig
-
-	err = yaml.Unmarshal(file, &c)
+	var builtInConf RsCliConfig
+	err := yaml.Unmarshal(builtInConfig, &builtInConf)
 	if err != nil {
 		return nil, errors.Wrap(err, "error parsing config file")
 	}
 
-	return &c, nil
+	envConf := getConfigFromEnvironment()
+
+	out := mergeConfigs(envConf, builtInConf)
+
+	cfgFilePath := getConfigPathFromArgs()
+	if cfgFilePath == "" {
+		cfgFilePath = getConfigPathFromExecutable()
+	}
+
+	if cfgFilePath == "" {
+		cfgFilePath = getConfigPathFromEnvironment()
+	}
+
+	file, err := os.ReadFile(cfgFilePath)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return nil, errors.Wrap(err, "error reading file from FS")
+	}
+
+	if len(file) == 0 {
+		return &out, nil
+	}
+
+	var externalConf RsCliConfig
+	err = yaml.Unmarshal(file, &externalConf)
+	if err != nil {
+		return nil, errors.Wrap(err, "error unmarshalling config from: "+cfgFilePath)
+	}
+
+	out = mergeConfigs(externalConf, out)
+
+	return &out, nil
+}
+
+func getConfigFromEnvironment() (r RsCliConfig) {
+	r.Env.PathToMain = os.Getenv(pathToMain)
+	r.Env.PathToConfig = os.Getenv(pathToConfig)
+
+	return
+}
+
+func getConfigPathFromArgs() string {
+	cfgFilePath, _ := flag.ExtractOneValueFromFlags(flag.ParseArgs(os.Args[1:]), customPathToConfig)
+	return cfgFilePath
+}
+
+func getConfigPathFromExecutable() string {
+	exePath, _ := os.Executable()
+	return path.Join(path.Dir(exePath), configFilename)
+}
+
+func getConfigPathFromEnvironment() string {
+	return os.Getenv(environmentPathToConfig)
+}
+
+func mergeConfigs(master, slave RsCliConfig) RsCliConfig {
+	if master.Env.PathToMain == "" {
+		master.Env.PathToMain = slave.Env.PathToMain
+	}
+
+	if master.Env.PathToConfig == "" {
+		master.Env.PathToConfig = slave.Env.PathToConfig
+	}
+
+	return master
 }
