e8e0a30004156252b700c8c2eb93a9aa978f2960
AlexSkilled <olegthander@yandex.ru>
2023-07-22 21:53:50 +0300

    [RSI-116]: instancer scripts is removed

--- plugins/instances/scripts/setup-vpn.sh
+++
@@ -1,7 +0,0 @@
-echo "This script requires sudo access"
-
-export HOSTNAME=hostname
-export API_PORT=port_for_api_number
-export KEYS_PORT=port_for_keys_number
-
-sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" "" --hostname "$HOSTNAME" --api-port $API_PORT --keys-port $KEYS_PORT
