9b1cfb0a5fca73a5df54152f39b6a537beb2d2f2
AlexSkilled <olegthander@yandex.ru>
2023-09-24 16:13:27 +0300

    moving closer

--- src/sections/header/header.tsx
+++ src/sections/header/header.tsx
@@ -1,15 +1,21 @@
 import cls from './header.module.css'
-import {Burger} from "../../components/burger/burger.tsx";
+
+import { motion } from 'framer-motion';
+
+import {Burger} from '../../components/burger/burger.tsx';
 
 export function Header(props: { setIsOpen: (isOpen: boolean) => void; isOpen: boolean; }) {
     return (
         <header className={cls.Header}>
-            <div className={cls.BurgerContainer}
+            <motion.div
+                layout
+                className={cls.BurgerContainer}
+                data-isOpen={props.isOpen}
                  onClick={()=> props.setIsOpen(!props.isOpen)}>
                 <Burger
                     isOpen={props.isOpen}
                 />
-            </div>
+            </motion.div>
         </header>
     );
 }
