db8a992eca9b26f565b8c3dd18b749f194c1f4dd
AlexSkilled <olegthander@yandex.ru>
2023-09-24 21:24:53 +0300

    thing getting annoing

--- src/sections/footer/footer.tsx
+++ src/sections/footer/footer.tsx
@@ -1,10 +1,26 @@
 import cls from './footer.module.css';
 
-export function Footer() {
-    return (
-        <div className={cls.Footer}>
+import {useState} from "react";
 
-        </div>
+import {motion} from 'framer-motion';
+
+export function Footer() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <motion.div
+            layout
+            className={cls.Footer}
+            data-isOpen={isOpen}
+        >
+            <div
+                onClick={() => setIsOpen(!isOpen)}
+                className={cls.OpenButton}>
+                <div className={cls.OpenButtonLeftLine}/>
+                <div className={cls.OpenButtonRightLine}/>
+
+            </div>
+        </motion.div>
     );
 }
 
