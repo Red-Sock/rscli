e6d702789e02e9091302ab80561f9e1978a766b9
AlexSkilled <olegthander@yandex.ru>
2023-09-25 00:06:22 +0300

    sidebar look kinda cooler

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,17 +3,14 @@ import cls from './home.module.css';
 import {useState} from "react";
 
 import {Header} from "../../sections/header/header.tsx";
-import {Footer} from "../../sections/footer/footer.tsx";
-import {Sidebar} from "../../sections/sidebar/sidebar.tsx";
-import {ContentDisplay} from "../../sections/content/content.tsx";
+import {ContentWrapper} from "../../sections/content/content.tsx";
 
 export function Home() {
     const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
     return (
         <div className={cls.Home}>
             <div className={cls.contentWrap}>
-                <ContentDisplay/>
-                <Sidebar
+                <ContentWrapper
                     isOpen={isSideMenuOpen}
                 />
             </div>
@@ -22,7 +19,6 @@ export function Home() {
                 isOpen={isSideMenuOpen}
                 setIsOpen={setIsSideMenuOpen}
             />
-            <Footer/>
         </div>
     )
 }
--- src/sections/content/content.tsx
+++ src/sections/content/content.tsx
@@ -1,8 +1,22 @@
 import cls from './content.module.css'
-export function ContentDisplay  () {
+
+import ReactMarkdown from 'react-markdown'
+import {Footer} from "../footer/footer.tsx";
+import {Sidebar} from "../sidebar/sidebar.tsx";
+
+
+export function ContentWrapper  (props: { isOpen: any; }) {
     return (
-        <div className={cls.ContentDisplay}>
-            <h1>Заголовок</h1>
+        <div className={cls.ContentWrapper}>
+            <div className={cls.Content}>
+                <ReactMarkdown>
+                    ## 2
+                </ReactMarkdown>
+
+                <Sidebar isOpen={props.isOpen}/>
+            </div>
+
+            <Footer/>
         </div>
     );
 }
