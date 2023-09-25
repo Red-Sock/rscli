2e122404933316c40354d6b1a8f52c8db82e2c46
AlexSkilled <olegthander@yandex.ru>
2023-09-25 10:30:38 +0300

    new color schema

--- src/sections/content/content.tsx
+++ src/sections/content/content.tsx
@@ -8,11 +8,12 @@ import {Sidebar} from "../sidebar/sidebar.tsx";
 export function ContentWrapper  (props: { isOpen: any; }) {
     return (
         <div className={cls.ContentWrapper}>
-            <div className={cls.Content}>
-                <ReactMarkdown>
-                    ## 2
-                </ReactMarkdown>
-
+            <div className={cls.ContentField}>
+                <div className={cls.Content}>
+                    <ReactMarkdown>
+                        ## 2
+                    </ReactMarkdown>
+                </div>
                 <Sidebar isOpen={props.isOpen}/>
             </div>
 
