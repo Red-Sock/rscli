35d90fc319bfc3f674a2730245c999ed0ede5567
AlexSkilled <olegthander@yandex.ru>
2023-09-26 20:47:11 +0300

    sidebar with open-closed elements

---
+++ src/components/tree/foldedIcon.tsx
@@ -0,0 +1,11 @@
+import {memo} from "react";
+
+export const FoldedIcon = memo(() => {
+        return (
+            <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
+                <path d="M0 0h24v24H0z" fill="none"/>
+                <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/>
+            </svg>
+        )
+    }
+)
--- src/sections/sidebar/sidebar.tsx
+++ src/sections/sidebar/sidebar.tsx
@@ -1,5 +1,9 @@
 import { motion } from 'framer-motion';
 import cls from './sidebar.module.css'
+import {TreeItem, TreeView} from "@mui/x-tree-view";
+
+import {FoldedIcon} from "../../components/tree/foldedIcon.tsx";
+import {UnfoldedIcon} from "../../components/tree/unfoldedIcon.tsx";
 export function Sidebar (props: { isOpen: any; }) {
 
     return (
@@ -9,10 +13,22 @@ export function Sidebar (props: { isOpen: any; }) {
             data-isOpen={props.isOpen}
         >
             <div className={cls.Content}>
-                <ul>
-                    <li>123faefavtrsvtreagrvaerfgerg</li>
-                    <li>1234</li>
-                </ul>
+               <TreeView
+                   aria-label="Documentation"
+                   defaultCollapseIcon={<FoldedIcon />}
+                   defaultExpandIcon={<UnfoldedIcon />}
+                   sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+               >
+                   <TreeItem nodeId="1" label="Applications">
+                       <TreeItem nodeId="2" label="Calendar" />
+                   </TreeItem>
+                   <TreeItem nodeId="5" label="Documents">
+                       <TreeItem nodeId="10" label="OSS" />
+                       <TreeItem nodeId="6" label="MUI">
+                           <TreeItem nodeId="8" label="index.js" />
+                       </TreeItem>
+                   </TreeItem>
+               </TreeView>
             </div>
         </motion.div>
     );
