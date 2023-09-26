6b9a87d0671af0dadcfaac92c6edc0676f6d0f84
AlexSkilled <olegthander@yandex.ru>
2023-09-26 20:33:52 +0300

    search

--- src/sections/header/header.tsx
+++ src/sections/header/header.tsx
@@ -3,6 +3,7 @@ import cls from './header.module.css'
 import {Burger} from '../../components/burger/burger.tsx';
 import {Sidebar} from "../sidebar/sidebar.tsx";
 import {useState} from "react";
+import {Search} from "../../components/search/search.tsx";
 
 export function Header() {
     const [isSideMenuOpen, setIsSideMenuOpen] = useState(false)
@@ -19,6 +20,9 @@ export function Header() {
             <Sidebar
                 isOpen={isSideMenuOpen}
             />
+            <div className={cls.SearchContainer}>
+                <Search/>
+            </div>
         </header>
     );
 }
