5e9242eb0959868ab07ce5b58576057f3e725012
AlexSkilled <olegthander@yandex.ru>
2023-09-27 14:11:57 +0300

    opening and redirecting to files

--- src/sections/content/content.tsx
+++ src/sections/content/content.tsx
@@ -5,7 +5,7 @@ import {memo, useState} from "react";
 
 
 export const ContentWrapper = memo(() => {
-        const contentURL = "https://red-sock.github.io";
+        const contentURL = "https://raw.githubusercontent.com/Red-Sock/rscli/gh-pages/docs/";
         const projectPath = "rscli";
         const docsFolder = "docs"
         const fileExtension = ".md"
@@ -16,7 +16,7 @@ export const ContentWrapper = memo(() => {
 
 
         const [content, setContent] = useState("")
-        const resourceURL = [contentURL, projectPath, docsFolder, resourcePath+fileExtension].join("/")
+        const resourceURL = [contentURL, resourcePath+fileExtension].join("/")
 
         fetch(resourceURL).then(async (response) => {
             if (response.ok) {
