12db1caa1ab97ef2ed90c91bb8d7aac5ef815427
AlexSkilled <olegthander@yandex.ru>
2023-12-08 15:40:10 +0300

    [dev]: compiling

--- plugins/project/projpatterns/pattern_c/grpc.mk
+++ plugins/project/projpatterns/pattern_c/grpc.mk
@@ -1,8 +1,6 @@
 gen: deps gen-server
 
 deps:
-	go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway@latest
-	go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
