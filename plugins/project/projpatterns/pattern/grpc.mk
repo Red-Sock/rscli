595f486875bbb10acfc6e57e8d3f8248d4b74651
AlexSkilled <olegthander@yandex.ru>
2023-12-15 18:17:40 +0300

    [RSI-219]: gateway generation

--- plugins/project/projpatterns/pattern/grpc.mk
+++ plugins/project/projpatterns/pattern/grpc.mk
@@ -1,14 +1,27 @@
-gen: deps gen-server
+gen: deps link-gw gen-server
 
 deps:
 	go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+	go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger@latest
+	go install github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway@latest
+
+link-gw:
+	ln -s $(GOPATH)/pkg/mod/github.com/grpc-ecosystem/grpc-gateway@v1.16.0/third_party/googleapis/google api/google
+	ln -s $(GOPATH)/pkg/mod/github.com/grpc-ecosystem/grpc-gateway
 
 gen-server: .pre-gen-server .gen-server
+
 .pre-gen-server:
 	mkdir -p pkg/
 
 .gen-server:
-	protoc --go_out=./pkg/ --go-grpc_out=./pkg/ \
-	--proto_path=. \
-	./api/*.proto
+	protoc \
+        	-I=./api \
+        	--grpc-gateway_out=logtostderr=true:./pkg/ \
+        	--swagger_out=allow_merge=true,merge_file_name=api:./api \
+    		--descriptor_set_out=./pkg/velez_api/api_descriptor.pb \
+        	--go_out=./pkg/ \
+        	--go-grpc_out=./pkg/ \
+        	./api/grpc/*.proto
+
