a79d8b6be8f57fd66f7a9b3b64f59a24a07dd06d
AlexSkilled <olegthander@yandex.ru>
2024-11-14 20:03:52 +0300

    [RSI-279] patterns to patterns

--- cmd/project/add/expected_test.go
+++ cmd/project/add/expected_test.go
@@ -17,9 +17,9 @@ var (
 	//go:embed expected/redis/config.yaml
 	expectedRedisConfig []byte
 
-	//go:embed expected/redis/config/data_source.go
+	//go:embed expected/redis/config/data_source.go.pattern
 	expectedRedisDataSourceConfig []byte
-	//go:embed expected/redis/app/data_source.go
+	//go:embed expected/redis/app/data_source.go.pattern
 	expectedRedisDataSourceApp []byte
 )
 
@@ -27,9 +27,9 @@ var (
 var (
 	//go:embed expected/postgres/config.yaml
 	expectedPostgresConfig []byte
-	//go:embed expected/postgres/config/data_sources.go
+	//go:embed expected/postgres/config/data_sources.go.pattern
 	expectedPostgresDataSourceConfig []byte
-	//go:embed expected/postgres/app/data_sources.go
+	//go:embed expected/postgres/app/data_sources.go.pattern
 	expectedPostgresDataSourceApp []byte
 )
 
@@ -37,21 +37,21 @@ var (
 var (
 	//go:embed expected/telegram/config.yaml
 	expectedTelegramConfig []byte
-	//go:embed expected/telegram/app/data_sources.go
+	//go:embed expected/telegram/app/data_sources.go.pattern
 	expectedTelegramDataSourcesApp []byte
-	//go:embed expected/telegram/config/data_sources.go
+	//go:embed expected/telegram/config/data_sources.go.pattern
 	expectedTelegramDataSourcesConfig []byte
-	//go:embed expected/telegram/transport/listener.go
+	//go:embed expected/telegram/transport/listener.go.pattern
 	expectedTelegramServer []byte
-	//go:embed expected/telegram/transport/handler/handler.go
+	//go:embed expected/telegram/transport/handler/handler.go.pattern
 	expectedTelegramServerHandlerExample []byte
 )
 
 var (
 	//go:embed expected/sqlite/config.yaml
 	expectedSqliteConfig []byte
-	//go:embed expected/sqlite/app/data_sources.go
+	//go:embed expected/sqlite/app/data_sources.go.pattern
 	expectedSqliteDataSourcesApp []byte
-	//go:embed expected/sqlite/config/data_sources.go
+	//go:embed expected/sqlite/config/data_sources.go.pattern
 	expectedSqliteDataSourcesConfig []byte
 )
